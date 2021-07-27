d27a81f6fe648dbbdffac1df8b468bdb811e0417
Andrii Guselietov <guseletov@gmail.com>
2021-07-27 11:02:16 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,17 +3,17 @@ provider "aws" {
 }
 
 
-#resource "aws_instance" "web" {
-#  ami           = "ami-0050dc92851799082"
-#  instance_type = "t3.micro"
-#
-#  tags = {
-#    "Name"      = "web01"
-#    "andriitag" = "true"
-#    "Cost_Centre" = "jamaice-dept-A"
-#  }
-#
-#}
+resource "aws_instance" "web" {
+  ami           = "ami-0050dc92851799082"
+  instance_type = "t3.micro"
+
+  tags = {
+    "Name"      = "web01"
+    "andriitag" = "true"
+    "Cost_Centre" = "jamaice-dept-A"
+  }
+
+}
 
 
 data "aws_caller_identity" "current" {}
